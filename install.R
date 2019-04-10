835ecd0e784cb00b3fea42ba2b2ca3b7e0b16090
krassowski <krassowski.michal@gmail.com>
2019-04-10 03:41:30 +0100

    Improve protein PCA, split molecular data analyses
    NB: some more improvements are planned to the protein data PCA:
     - use of Q^2 criterion for # of PC selection
     - reanalysis/reworking of the plots which currently present
       first four PCs, though fail to show relations between
       all the PC pairs
    
    Other changes in this commit:
     - annotations & colors are now stored in plain python/R files,
       allowing to share the color scheme across notebooks & analyses
     - added some notes on PCA with prcomp and factoextra
     - added missing R dependencies

--- install.R
+++ install.R
@@ -1,5 +1,8 @@
+install.packages('ggplot2')
 install.packages('pheatmap')
 install.packages('qqplotr')
+install.packages('cowplot')
+install.packages('ggthemes')
 
 source("https://bioconductor.org/biocLite.R")
 biocLite("readat")
--- plots/pca.R
+++ plots/pca.R
@@ -1,14 +1,6 @@
-small_heatmap = function(matrix) {
-    colnames(matrix) <- sub('^X', '', colnames(matrix))
-    pheatmap(
-        matrix,
-        show_colnames=FALSE,
-        annotation_col=patient_annotations,
-        annotation_colors=patient_colors,
-        border_color='grey70',
-        scale='row'
-    )
-}
+library(ggplot2)
+source('plots/colors.R')
+
 small_pca = function(matrix, name='', axes=c(1, 2)) {
     colnames(matrix) <- sub('^X', '', colnames(matrix))
     pca = prcomp(t(matrix), scale=T)
@@ -20,6 +12,7 @@ small_pca = function(matrix, name='', axes=c(1, 2)) {
             gradient.cols=c("#c49c94", "#2CA02C"),
             select.var=list(contrib=25),
             col.ind='white',
+            # TODO: generalise
             fill.ind=patient_annotations$Meningitis,
             palette=patient_colors$Meningitis,
             pointshape=21, pointsize=3,
@@ -37,6 +30,7 @@ small_pca = function(matrix, name='', axes=c(1, 2)) {
 }
 
 first_four_pcs = function(matrix, name) {
+    # TODO: rewrite
     a = small_pca(matrix, axes=c(1, 2), name=name)
     b = small_pca(matrix, axes=c(3, 4), name=name)
     cowplot::plot_grid(
@@ -52,5 +46,17 @@ first_four_pcs = function(matrix, name) {
         cowplot::get_legend(a + guides(color=F)),
         nrow=3,
         rel_heights=c(1, .08, .06)
+    )
+}
+
+cumulative_variance_explained <- function(data, threshold){
+    # note: preferred way to select number of PC would be to use CV,
+    # though this gives a nice overwiew of the PCs importance anyway
+    (
+        ggplot(data, aes(x=PC, y=cumulative.variance.percent))
+        + geom_bar(stat='identity', fill='grey70')
+        + annotate('segment', x=0, xend=Inf, y=threshold, yend=threshold, colour="red")
+        + theme(legend.position='none', axis.text.x=element_text(angle=90))
+        + xlab('Prinicpal component') + ylab('Cumulative variance explained [%]')
     )
 }
