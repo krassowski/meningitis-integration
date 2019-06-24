687f24de742387f8840b427ddfcb0214475d2681
krassowski <krassowski.michal@gmail.com>
2019-06-24 10:44:39 +0100

    Add rpy2 function helper with sensible conversion

--- helpers/r.py
+++ helpers/r.py
@@ -1,7 +1,17 @@
 from rpy2.robjects import r
+from rpy2.robjects.conversion import localconverter
+from rpy2.robjects import numpy2ri
+import rpy2.robjects as ro
+from rpy2.robjects import pandas2ri
 
 
 def p_adjust(x, *args, **kwargs):
     if not isinstance(x, list):
         x = list(x)
     return r['p.adjust'](x, *args, **kwargs)
+
+
+def r_function(__name__, *args, **kwargs):
+    with localconverter(ro.default_converter + numpy2ri.converter + pandas2ri.converter):
+        return r[__name__](*args, **kwargs)
+
