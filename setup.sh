0e7e0d299faf050dbffb20725173bbe4c120f298
krassowski <krassowski.michal@gmail.com>
2019-06-24 10:39:01 +0100

    Add venn diagrams support (Python side, based on pyvenn)

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@
 sudo apt-get install libproj-dev libgdal-dev  # Needed for some R dependencies
 
 pip3 install -r requirements.txt
+cd helpers
+wget https://raw.githubusercontent.com/krassowski/pyvenn/master/venn.py -o _venn.py
+cd ..
 Rscript install.R
