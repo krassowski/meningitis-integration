f2680cd31c6519288e7b7c4f2302ba41562a7242
krassowski <krassowski.michal@gmail.com>
2019-07-10 12:19:01 +0100

    Add colors for RNA outliers

--- plots/colors.R
+++ plots/colors.R
@@ -5,13 +5,16 @@ nice_theme = list(theme_bw(), theme(legend.position='bottom'))
 patient_colors <- list(
     'HIV status'=brewer.pal(2, "Set3")[1:2],
     'Tuberculosis status'=rev(brewer.pal(4, "Greens")),
-    'Meningitis'=ggthemes::tableau_color_pal('Tableau 10')(5)
+    'Meningitis'=ggthemes::tableau_color_pal('Tableau 10')(5),
+    'Is RNA outlier'=c('grey95', 'red')
+
 )
 
 patient_colors_values <- list(
     'HIV status'=c('Positive', 'Negative'),
     'Tuberculosis status'=c('Definite', 'Probable', 'Possible', '-'),
-    'Meningitis'=c('Tuberculosis', 'Cryptococcal', 'Viral', 'Healthy control', 'Bacterial')
+    'Meningitis'=c('Tuberculosis', 'Cryptococcal', 'Viral', 'Healthy control', 'Bacterial'),
+    'Is RNA outlier'=c('False', 'True')
 )
 
 for(group in names(patient_colors)) {
