0fc535e7315affc244d4cde119cc7db121db5b1f
krassowski <krassowski.michal@gmail.com>
2019-07-16 13:29:55 +0100

    Use R 3.5.2

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install libproj-dev libgdal-dev  # Needed for some R dependencies
-sudo apt-get install r-base
+sudo apt-get install r-base==3.5.2
 sudo apt-get install graphviz libgraphviz-dev graphviz-dev # (not used directly, required for static plots of nbpipeline)
 
 pip3 install -r requirements.txt
