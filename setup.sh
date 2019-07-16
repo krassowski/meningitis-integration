58c8bd0bdc0c8c49f5b9b35b94daef70104710e2
krassowski <krassowski.michal@gmail.com>
2019-07-16 15:07:02 +0100

    Debug 2

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # see https://askubuntu.com/questions/1056630/r-3-5-0-not-working-on-ubuntu-18-04
+sudo apt-get update
 cat <<EOF | sudo tee /etc/apt/preferences.d/pin-r35
 Package: r-*
 Pin: release a=bionic-cran35
@@ -13,8 +14,6 @@ Pin: version 3.1.139-1bionic0
 Pin-Priority: 800
 EOF
 
-sudo apt policy r-recommended
-sudo apt-get update
 sudo apt-get install libproj-dev libgdal-dev  # Needed for some R dependencies
 sudo apt-get install r-base r-base-dev
 sudo apt-get install graphviz libgraphviz-dev graphviz-dev # (not used directly, required for static plots of nbpipeline)
