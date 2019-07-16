d2cafaaaa339c6e92367c5b57e664ff68c40cfe2
krassowski <krassowski.michal@gmail.com>
2019-07-16 17:58:24 +0100

    Try quiet installs

--- install.R
+++ install.R
@@ -1,27 +1,27 @@
-install.packages('ggplot2')
-install.packages('pheatmap')
-install.packages('qqplotr')
-install.packages('cowplot')
-install.packages('ggthemes')
+install.packages('ggplot2', quiet=T)
+install.packages('pheatmap', quiet=T)
+install.packages('qqplotr', quiet=T)
+install.packages('cowplot', quiet=T)
+install.packages('ggthemes', quiet=T)
 
-install.packages('limma')
-install.packages('statmod')
+install.packages('limma', quiet=T)
+install.packages('statmod', quiet=T)
 
-install.packages('parallel')
+install.packages('parallel', quiet=T)
 #install.packages('pbmcapply')
 
-install.packages('import')
+install.packages('import', quiet=T)
 
 source("https://bioconductor.org/biocLite.R")
-biocLite("readat")
-install.packages('gprofiler2')
+biocLite("readat", quiet=T)
+install.packages('gprofiler2', quiet=T)
 
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 BiocManager::install("DESeq2")
-install.packages('corrplot')
+install.packages('corrplot', quiet=T)
 BiocManager::install("IHW")
 
 install.packages('devtools')
@@ -31,8 +31,8 @@ Sys.unsetenv("GITHUB_PAT")
 # BiocManager::install("ComplexHeatmap")
 devtools::install_github("jokergoo/ComplexHeatmap")
 
-install.packages("pvclust")
-install.packages("ggstatsplot")
+install.packages("pvclust", quiet=T)
+install.packages("ggstatsplot", quiet=T)
 
 BiocManager::install("mixOmics")
 BiocManager::install("ropls")
