06ca8de01e90eed51eb1cf5c4c0e67cf3ce53539
krassowski <krassowski.michal@gmail.com>
2019-07-17 13:43:35 +0100

    Revert pip changes

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo apt-get install libproj-dev libgdal-dev  # Needed for some R dependencies
 sudo apt-get install graphviz libgraphviz-dev graphviz-dev # (not used directly, required for static plots of nbpipeline)
 
-pip3 install -r -U requirements.txt
+pip3 install -r requirements.txt
 cd helpers
 wget https://raw.githubusercontent.com/krassowski/pyvenn/master/venn.py -o _venn.py
 cd ..
