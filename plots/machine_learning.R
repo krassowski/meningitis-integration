60bb8454f026eb4f644502c9f75337bc34dfe3bc
krassowski <krassowski.michal@gmail.com>
2019-08-13 22:18:53 +0100

    Futher improve the AUC/ROC plot

--- plots/machine_learning.R
+++ plots/machine_learning.R
@@ -263,23 +263,36 @@ coefficients_volcano_plot = function(coeffs, n=10, transformation=identity, p_va
     )
 }
 
+                                                     
+# function from documentation of tableGrob
+# which is authored by Baptiste Auguie and GPL (≥ 2) licences
+# https://cran.r-project.org/web/packages/gridExtra/vignettes/tableGrob.html 
+find_cell <- function(table, row, col, name="core-fg") {
+    l <- table$layout
+    which(l$t==row & l$l==col & l$name==name)
+}
+
 
 plot_roc_auc = function(
     roc_auc, random_ci=FALSE, annotate_lines=FALSE,
     annotation_x=0.5, ci=TRUE, mean_auc=FALSE,
     annotation_at=0.05, annotation_nudge_x=NULL,
-    label=TRUE, table=FALSE
+    label=TRUE, table=FALSE, table_xmin=0.4, table_xmax=0.75,
+    color='group', linetype=NULL,
+    random_linetype=2, random_color='red', table_row_colors=NULL,
+    nudge_x='auto', ...
 ) {
     
     if(is.null(roc_auc$group))
         roc_auc$group = 'AUC'
     
-    grouped = aggregate(roc_auc, list(group_name=roc_auc$group), mean)
+    grouped = aggregate(roc_auc, list(group_name=roc_auc$group, color=roc_auc[[color]]), mean)
     grouped$group = grouped$group_name
+    grouped[[color]] = grouped$color
     
     p = (
         ggplot(roc_auc, aes(x=x_linear_space))
-        + geom_line(aes(y=random_expected), color='red', linetype=2)
+        + geom_line(aes(y=random_expected), color=random_color, linetype=random_linetype)
     )
     
     if (random_ci)
@@ -291,7 +304,12 @@ plot_roc_auc = function(
     if (ci)
         p = (p
             + geom_ribbon(
-                aes(ymin=true_positive_lower_ci, ymax=true_positive_upper_ci, fill=group),
+                aes_string(
+                    ymin='true_positive_lower_ci',
+                    ymax='true_positive_upper_ci',
+                    fill=color,
+                    group='group'
+                ),
                 alpha=0.1
             )
         )
@@ -307,41 +325,53 @@ plot_roc_auc = function(
     )$x
 
     annotate_line = if (label) ggrepel::geom_label_repel else ggrepel::geom_text_repel
-    
-    if (annotate_lines)
+
+    if (annotate_lines) {
+        if(nudge_x == 'auto') {
+            nudge_x = (
+                if(!is.null(annotation_nudge_x))
+                    annotation_nudge_x
+                else
+                    min(lowest_fpr) - 0.05
+            )
+        }
         p = (p
             + annotate_line(
                 data=grouped,
-                aes(
-                    x=annotation_x,
-                    label=paste0(
+                aes_string(
+                    x='annotation_x',
+                    group='group',
+                    y='lowest_fpr',
+                    label="paste0(
                         group,
                         ', AUC: ',
                         round(pooled_auc, 3),
                         ' \U00B1 ',
                         round(std_auc, 2)
-                    ),
-                    y=lowest_fpr,
-                    color=group
+                    )",
+                    color=color
                 ),
                 #direction='x',
                 hjust=1,
                 segment.alpha=0.4,
-                nudge_x=(
-                    if(!is.null(annotation_nudge_x))
-                        annotation_nudge_x
-                    else
-                        min(lowest_fpr) - 0.05
-                ),
-                key_glyph='blank'
+                nudge_x=nudge_x,
+                key_glyph='blank',
+                ...
                 #nudge_y=c(0,)
                 ## - subset(dat, wt > 3)$wt
             )
         )
+    }
     
     
     p = (p
-        + geom_line(aes(y=true_positive_mean, color=group), key_glyph='timeseries')
+        + geom_line(
+            aes_string(
+                y='true_positive_mean', group='group',
+                color=color, linetype=linetype
+            ),
+            key_glyph='timeseries'
+        )
     )
     if (mean_auc) {
         p = (
@@ -366,27 +396,53 @@ plot_roc_auc = function(
             round(grouped$average_auc_ci_lower, 3), '-', round(grouped$average_auc_ci_upper, 3)
         )
         colnames(df) = c('AUC', 'AUC', 'AUC 95% CI')
-        
         rownames(df) = grouped$group
+        
+        if(!is.null(table_row_colors)) {
+            # note this very muh does not work due to some kind of bug in tableGrob,
+            # a workaround is defined below
+            col = table_row_colors[grouped$color]
+            col = as.vector(col)
+        }
+        else {
+            col='black'
+        }
+        
         table_theme = gridExtra::ttheme_minimal(
-            core = list(fg_params=list(cex = 0.9), bg_params=list(fill='grey97')),
+            core = list(
+                fg_params=list(cex = 0.9),
+                bg_params=list(fill='grey97')
+            ),
             colhead = list(fg_params=list(cex = 0.9)),
-            rowhead = list(fg_params=list(cex = 0.9))
+            rowhead = list(fg_params=list(cex = 0.9, col=col, fontface=1))
         )
         table_grid = gridExtra::tableGrob(
             df, theme=table_theme
         )
+        
+        if(!is.null(table_row_colors)) {
+            i = 2
+            for(row_col in col) {
+                # row column, for our purpose starts at 2
+                ind <- find_cell(table_grid, i, 1, "rowhead-fg")
+                table_grid$grobs[ind][[1]][["gp"]] <- grid::gpar(col=row_col, cex=0.9)
+                i = i + 1
+            }
+        }
+        #print(table_grid)
+        #print(df[1:2, 1:2])
         header <- gridExtra::tableGrob(
-            df[1, 1:2], theme=table_theme,
-            rows=NULL, cols=c('Pooled', 'Averaged')
+            df[, 1:2], theme=table_theme,
+            #rows=rownames(df),
+            cols=c('Pooled', 'Averaged')
         ) 
 
         merged_table <- gridExtra::combine(header[1,], table_grid, along=2)
         merged_table$layout[1:4 , c("l", "r")] <- list(c(2, 3), c(2, 4))
-        
+
         p = p + annotation_custom(
             merged_table,
-            xmin=0.4, xmax=0.75, ymin=0.05 * nrow(df) + 0.07, ymax=0
+            xmin=table_xmin, xmax=table_xmax, ymin=0.05 * nrow(df) + 0.07, ymax=0
         )
     }
     
@@ -512,4 +568,25 @@ plot_contributions_network = function(
             color=guide_legend(override.aes=list(size=4))
         )
     )
+}
+
+                                                     
+add_linetype_to_ggrepel = function(g, linetypes, modify='segment') {
+    library(grid)
+    g = grid.force(g)
+
+    labels <- grid.ls(
+        getGrob(g, c("GRID.labelrepelgrob"), grep=TRUE, global=TRUE),
+        print=F
+    )$name
+
+    for (i in 1:(length(labels) / 4)) {
+        g = editGrob(
+            grid.force(g),
+            c(labels[[4*(i-1)+1]], modify), grep = TRUE,
+            gp = gpar(lty=linetypes[[i]]),
+            global=T
+        )
+    }
+    g
 }
