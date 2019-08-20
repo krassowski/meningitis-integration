c19145fa74ede493e3b4b232c01d2a4214ee213b
krassowski <krassowski.michal@gmail.com>
2019-08-21 00:43:46 +0100

    Add a utility for plot space usage maximization

--- plots/utils.R
+++ plots/utils.R
@@ -10,3 +10,34 @@ label_outliers = function(label, value, group, outlier_test=is_outlier) {
     ]
     ifelse(ids %in% outliers, as.character(label), NA)
 }
+
+
+# For a plot which has randomly placed elements
+# (for example based on the force directed layout)
+# iterate over seeds and find the starting point which maximizes
+# space usage, assuming that the background is white  
+optimize_space_usage = function(plotting_func, end, start=1) {
+    library(imager)
+
+    whites_count = 0
+    count_whites = function(r,g,b) {
+        whites_count <<- sum(r== 1 & r == g & g == b)    
+        rgb(r,g,b)
+    }
+
+    whites = list()
+
+    for (i in start:end) {
+        set.seed(i)
+
+        p = plotting_func()
+        suppressMessages(ggsave('.temp.png', p, dpi=80, w=10, h=7, scale=1.2))
+
+        img = load.image('.temp.png')
+        img = as.raster(img, colourscale=count_whites, rescale=F)
+
+        whites[i] = whites_count
+    }
+
+    whites
+}
