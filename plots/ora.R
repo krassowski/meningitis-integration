0039c276377f3de6349e051db5802af03276f154
krassowski <krassowski.michal@gmail.com>
2020-04-13 21:19:12 +0100

    Re-run differential levels notebook:
    - do not attempt to get non-significant results from gprofiler, this
      only lead to R hanging (too much data to download)
    - update normalize_abundance calls as it no longer accepts file argument
    - set max.overlaps=Inf for new ggrepel version
    - clean up whitespaces

--- plots/ora.R
+++ plots/ora.R
@@ -1,29 +1,28 @@
 penalise_for_chance_in_background_selection = function(with_bg, without_bg) {
     bg_and_no_bg = merge(with_bg$result, without_bg$result, by='term_name', suffixes = c(".bg", ".all"))
     bg_and_no_bg$ratio_studied = bg_and_no_bg$term_size.bg / bg_and_no_bg$term_size.all
-    bg_and_no_bg$chance_of_inclusion_in_SOMAScan.p_value = unlist(apply(bg_and_no_bg, 1, function(x){
+    bg_and_no_bg$chance_of_inclusion_in_SOMAScan.p_value = unlist(apply(bg_and_no_bg, 1, function(x) {
         binom.test(as.numeric(x['term_size.bg']), as.numeric(x['term_size.all']), alternative='greater')$p.value
     }))
     # this is no longer easily interpretable p-value (!)
     bg_and_no_bg$weigted_p = bg_and_no_bg$p_value.bg / (1 - bg_and_no_bg$chance_of_inclusion_in_SOMAScan.p_value)
-    bg_and_no_bg = bg_and_no_bg[order(bg_and_no_bg$weigted_p),]
+    bg_and_no_bg = bg_and_no_bg[order(bg_and_no_bg$weigted_p), ]
     # high_confidence_subset = bg_and_no_bg[bg_and_no_bg$significant.all == T,]
-    
+
     bg_and_no_bg
 }
 
 abbreviate_terms = function(df, abbreviations) {
-    if(!is.null(abbreviations)) {
+    if (!is.null(abbreviations)) {
         df$term_name = stringr::str_replace_all(df$term_name, abbreviations)
     }
     df
 }
 
 highlight_keywords = function(df, keywords) {
-    if(!is.null(keywords)) {
-        for (keyword_group in keywords){
-            for (keyword in keyword_group)
-            {
+    if (!is.null(keywords)) {
+        for (keyword_group in keywords) {
+            for (keyword in keyword_group) {
                 k = paste0(c('\\\\textbf{', keyword, '}'), collapse='')
                 df$term_name = gsub(keyword, k, df$term_name)
             }
@@ -50,9 +49,9 @@ annotated_manhattan = function(gprofiler_ora_result, n=15, abbreviations=NULL, k
 
     df = abbreviate_terms(df, abbreviations)
     df = highlight_keywords(df, keywords)
-    
+
     ymax = -log10(min(df$p_value / 2))
-    
+
     p = (
         p
         + ggrepel::geom_text_repel(
@@ -70,7 +69,7 @@ chance_plot = function(with_bg, without_bg, n=15, abbreviations=NULL, keywords=N
     set.seed(seed)
 
     weighted_by_likelihood = penalise_for_chance_in_background_selection(with_bg=with_bg, without_bg=without_bg)
-    
+
     df = head(weighted_by_likelihood, n=n)
 
     df = abbreviate_terms(df, abbreviations)
@@ -87,6 +86,5 @@ chance_plot = function(with_bg, without_bg, n=15, abbreviations=NULL, keywords=N
             parse=T
         )
         + explain_abbreviations(abbreviations, x=Inf, y=Inf, hjust=1, vjust=1)
-
     )
 }
--- plots/volcano.R
+++ plots/volcano.R
@@ -1,18 +1,17 @@
-
 select_n_most_significant = function(data, n) {
-    head(data[order(data$adj.P.Val),], n)
+    head(data[order(data$adj.P.Val), ], n)
 }
 
 select_n_most_extreme = function(data, n) {
-    head(data[order(-abs(data$logFC)),], n)
+    head(data[order(-abs(data$logFC)), ], n)
 }
 
 select_n_most_extreme_two_sided = function(data, n) {
-    reduced_abundance = data[data$logFC < 0,]
-    increased_abundance = data[data$logFC > 0,]
+    reduced_abundance = data[data$logFC < 0, ]
+    increased_abundance = data[data$logFC > 0, ]
     rbind(
-        head(reduced_abundance[order(reduced_abundance$logFC),], n),
-        head(increased_abundance[order(-increased_abundance$logFC),], n)
+        head(reduced_abundance[order(reduced_abundance$logFC), ], n),
+        head(increased_abundance[order(-increased_abundance$logFC), ], n)
     )
 }
 
@@ -27,34 +26,36 @@ annotate_significance_thresholds = function(alpha, alpha_suggestive, transform=N
     )
 }
 
-volcano_plot = function(data, facet=F, n=15, select=select_n_most_significant, significant_only=T, scale='fixed', sig_threshold=0.05, transparency=0.4) {
-    
+volcano_plot = function(
+    data, facet=F, n=15, select=select_n_most_significant,
+    significant_only=T, scale='fixed', sig_threshold=0.05, transparency=0.4
+) {
+
     if (is.null(data$protein))
         data$protein = data$gene_name
-    #data$protein = rownames(data))
-    
+
     # set seed to increase labels positions reproducibility
     set.seed(0)
     data$is_significant = ifelse(data$adj.P.Val < sig_threshold, 'significant', 'non-significant')
-    significant = data[data$is_significant=='significant',]
-    
+    significant = data[data$is_significant=='significant', ]
+
     select_n_top <- function(x) { select(x, n=n) }
 
-    highlight_data = if(significant_only) significant else data
-    
-    if(facet != F){
+    highlight_data = if (significant_only) significant else data
+
+    if (facet != F) {
         x = by(highlight_data, highlight_data[[facet]], select_n_top)
         selected_proteins = do.call('rbind', x)
     } else {
         selected_proteins = select_n_top(highlight_data)
     }
-    label_color = sapply(selected_proteins$adj.P.Val, function(x){
+    label_color = sapply(selected_proteins$adj.P.Val, function(x) {
         if (x < 0.05) color = 'black'
         else if (x < 0.10) color = 'grey30'
         else color = 'grey60'
         color
     })
-    
+
     g = (
         ggplot(data, aes(x=logFC, y=-log10(adj.P.Val), color=is_significant))
         + theme_bw()
@@ -69,6 +70,7 @@ volcano_plot = function(data, facet=F, n=15, select=select_n_most_significant, s
             show.legend=F,
             color=label_color,
             segment.color='grey50',
+            max.overlaps=Inf,
             max.iter=50000, force=1, point.padding=0.3, box.padding=0.8, min.segment.length=0.2
         )
         + theme(legend.position='bottom')
