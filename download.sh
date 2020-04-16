012e0b4ad95a14dd51977b7d8a3d2c183d8d6b50
krassowski <krassowski.michal@gmail.com>
2020-04-16 01:45:15 +0100

    Add missing "entrez_to_gene_symbol.tsv", partial GSEA re-run

--- download.sh
+++ download.sh
@@ -21,6 +21,7 @@ zcat HUMAN_9606_idmapping.dat.gz | grep GeneID | cut -f 1,3 > uniprot_to_gene_id
 cd hgnc
 wget "https://www.genenames.org/cgi-bin/download/custom?col=gd_pub_ensembl_id&col=gd_pub_eg_id&status=Approved&hgnc_dbtag=on&order_by=gd_app_sym_sort&format=text&submit=submit" -O ensembl_gene_to_entrez.tsv
 python clean_ensembl_entrez_mapping.py
+wget "https://www.genenames.org/cgi-bin/download/custom?col=gd_pub_eg_id&col=gd_app_sym&status=Approved&hgnc_dbtag=on&format=text&submit=submit" -O entrez_to_gene_symbol.tsv
 cd ..
 
 
